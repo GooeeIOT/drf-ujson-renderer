4bde091acee64109e3b9dc5a71f8acdd02d7b77d
Lyle Scott, III <lyle@ls3.io>
2018-07-31 17:15:33 -0400

    serializer changes

--- drf_ujson/renderers.py
+++ drf_ujson/renderers.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 from rest_framework.compat import six
 from rest_framework.renderers import BaseRenderer
 import ujson
+from rest_framework.utils.encoders import JSONEncoder
+from django.utils.itercompat import is_iterable
 
 
 class UJSONRenderer(BaseRenderer):
@@ -15,13 +17,27 @@ class UJSONRenderer(BaseRenderer):
     format = 'json'
     ensure_ascii = True
     charset = None
+    encoder = JSONEncoder().default
+
+    def json_serializer(self, x):
+        """Convert Python types to JSON where necessary."""
+        if isinstance(x, (str, bool, int, float, None.__class__)):
+            # Basecase
+            return x
+        elif isinstance(x, dict):
+            return {k: self.json_serializer(v) for k, v in x.items()}
+        elif is_iterable(x):
+            return [self.json_serializer(item) for item in x]
+
+        return self.encoder(x)
 
     def render(self, data, *args, **kwargs):
 
         if data is None:
             return bytes()
 
-        ret = ujson.dumps(data, ensure_ascii=self.ensure_ascii)
+        ret = ujson.dumps(self.json_serializer(data, UJSONRenderer.encoder),
+                          ensure_ascii=self.ensure_ascii)
 
         # force return value to unicode
         if isinstance(ret, six.text_type):
